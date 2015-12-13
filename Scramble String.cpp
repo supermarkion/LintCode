dd1143349c56a1d93025ae4c92677fe8edd681bf
supermarkioner <supermarkioner@gmail.com>
2015-12-14 00:21:08 +1100

    Maybe the recursion approach is much easier

---
+++ Scramble String.cpp
@@ -0,0 +1,59 @@
+/*
+    Given a string s1, we may represent it as a binary tree by partitioning it to two non-empty substrings recursively.
+    Link: http://www.lintcode.com/en/problem/scramble-string/
+    Example: 
+    Solution: We apply the dynamic programming to solve this problem.
+    Source:
+        https://github.com/kamyu104/LintCode/blob/master/C++/scramble-string.cpp
+        http://www.cnblogs.com/TenosDoIt/p/3452004.html
+*/
+class Solution {
+public:
+    /**
+     * @param s1 A string
+     * @param s2 Another string
+     * @return whether s2 is a scrambled string of s1
+     */
+    bool isScramble(string& s1, string& s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        if (s1 == s2) {
+            return true;
+        }
+
+        const auto n = s1.length();
+
+        // is_cramble[l][i][j] denoted as s1[i:i+l], s2[j:j+l] are scramble.
+        vector<vector<vector<bool>>> 
+            is_scramble(n + 1, vector<vector<bool>>(n, vector<bool>(n, false)));
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (s1[i] == s2[j]) {
+                    is_scramble[1][i][j] = true;
+                }
+            }
+        }
+
+        // Compute by each length.
+        for (int l = 2; l <= n; ++l) {
+            for (int i = 0; i <  n - l + 1; ++i) {
+                for (int j = 0; j < n - l + 1; ++j) {
+                    for (int k = 1; k < l; ++k) {
+                        if ((is_scramble[k][i][j] &&
+                             is_scramble[l - k][i + k][j + k]) ||
+                            (is_scramble[k][i][j + l - k] &&
+                             is_scramble[l - k][i + k][j])) {
+                            is_scramble[l][i][j] = true;
+                            break; // Pruning.
+                        }
+                    }
+                }
+            }
+        }
+
+        return is_scramble[n][0][0];
+    }
+};
