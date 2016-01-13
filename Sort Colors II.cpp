bbee2d76a6290361ac35f14e08c3e524fb3a120e
supermarkioner <supermarkioner@gmail.com>
2016-01-13 16:48:33 +1100

    update

--- Sort Colors II.cpp
+++ Sort Colors II.cpp
@@ -1,25 +1,45 @@
 /*
-    Using O(1) time to check whether an integer n is a power of 2.
+    Given an array of n objects with k different colors (numbered from 1 to k), sort them so that objects of the same 
+    color are adjacent, with the colors in the order 1, 2, ... k.
 
-    Link: http://www.lintcode.com/en/problem/o1-check-power-of-2/
+    Link: http://www.lintcode.com/en/problem/sort-colors-ii/
 
-    Example: 
-        For n=4, return true;
-        For n=5, return false;
+    Example: Given colors=[3, 2, 2, 1, 4], k=4, your code should sort colors in-place to [1, 2, 2, 3, 4].
 
-    Solution: Check the last digit is 1 or not in binary style.
+    Solution: Perform bucket sort method.
 
-    Source: https://github.com/kamyu104/LintCode/blob/master/C%2B%2B/o1-check-power-of-2.cpp
+    Source: https://github.com/kamyu104/LintCode/blob/master/C%2B%2B/sort-colors-ii.cpp
 */
 
-class Solution {
+class Solution{
 public:
-    /*
-     * @param n: An integer
-     * @return: True or false
-     */
-    bool checkPowerOf2(int n) {
+    /**
+     * @param colors: A list of integer
+     * @param k: An integer
+     * @return: nothing
+     */    
+    void sortColors2(vector<int> &colors, int k) {
         // write your code here
-        return n > 0 && (n & (n - 1)) == 0;
+        for (int i = 0; i < colors.size(); ++i) {
+            if (colors[i] > 0) {
+                int pos = colors[i] - 1;
+                if (colors[pos] <= 0) {  // Bucket exists.
+                    --colors[pos];
+                    colors[i] = 0;
+                }
+                else {  // Init a new bucket.
+                    colors[i] = colors[pos];
+                    colors[pos] = -1;
+                    --i;
+                }
+            }
+        }
+
+        for (int i = colors.size() - 1, pos = k - 1; pos >= 0; --pos) {
+            while (colors[pos] < 0) {  // Reorder the color by count of each bucket.
+                ++colors[pos];
+                colors[i--] = pos + 1;
+            }
+        }
     }
 };
