7111485ca47e2e0c314c75ace563772294bcd008
supermarkioner <supermarkioner@gmail.com>
2016-01-22 10:39:22 +1100

    update

---
+++ Binary Tree Level Order Traversal II.cpp
@@ -0,0 +1,82 @@
+/*
+    Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, 
+    level by level from leaf to root).
+
+    Link: http://www.lintcode.com/en/problem/binary-tree-level-order-traversal-ii/
+
+    Example: Given binary tree {3,9,20,#,#,15,7},
+		    3
+		   / \
+		  9  20
+		    /  \
+		   15   7
+
+		return its bottom-up level order traversal as:
+		[
+		  [15,7],
+		  [9,20],
+		  [3]
+		]
+
+    Solution: Use reverse the result of part one problem.
+
+    Source: https://github.com/kamyu104/LintCode/blob/master/C%2B%2B/binary-tree-level-order-traversal-ii.cpp
+*/
+
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+ 
+ 
+class Solution {
+    /**
+     * @param root : The root of binary tree.
+     * @return : buttom-up level order a list of lists of integer
+     */
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode *root) {
+        // write your code here
+        vector<vector<int>> output;
+        queue<TreeNode *> que;
+        vector<int> cur_level;
+
+        // Enqueue when node is not nullptr.
+        if (root) {
+            que.emplace(root);
+        }
+
+        int cur_level_cnt = que.size();
+        while (!que.empty()) {
+            TreeNode *node = que.front();
+            que.pop();
+            --cur_level_cnt;
+
+            cur_level.emplace_back(node->val);
+
+            // Enqueue the next level.
+            if (node->left) {
+                que.emplace(node->left);
+            }
+            if (node->right) {
+                que.emplace(node->right);
+            }
+
+            // Current level has been all visited.
+            if (cur_level_cnt == 0) {
+                cur_level_cnt = que.size();
+                output.emplace_back(move(cur_level));
+            }
+        }
+        reverse(output.begin(), output.end());
+        return output;
+    }
+};
