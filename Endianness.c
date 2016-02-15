3601a33e30eb81838e2c17cb90a515173b5fec56
supermarkioner <supermarkioner@gmail.com>
2016-02-15 11:06:43 +1100

    update endianness problem in c language

---
+++ Endianness.c
@@ -0,0 +1,14 @@
+/* Returns 1 if the machine is little-endian, 0 if the 
+ * machine is big-endian
+ */
+
+int Endianness(void)
+{
+	union {
+		int theInteger;
+		char singleByte;
+	} endianTest;
+
+	endianTest, theInteger = 1;
+	return endianTest, singleByte;
+}
