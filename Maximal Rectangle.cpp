a85fa468fdee06bf4129ee7a47882923a979d88a
supermarkioner <supermarkioner@gmail.com>
2016-03-09 08:38:29 +1100

    update

---
+++ Maximal Rectangle.cpp
@@ -0,0 +1,19 @@
+/*
+    Given a 2D boolean matrix filled with False and True, find the largest rectangle containing all True and return its area.
+    
+    Link: http://www.lintcode.com/en/problem/maximal-rectangle/
+
+    Example: Given a matrix:
+		[
+		  [1, 1, 0, 0, 1],
+		  [0, 1, 0, 0, 1],
+		  [0, 0, 1, 1, 1],
+		  [0, 0, 1, 1, 1],
+		  [0, 0, 0, 0, 1]
+		]
+		return 6.
+
+    Solution: None
+
+    Source: None
+*/
