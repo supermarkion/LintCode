d899354ca1d5e8c0097f54be0cb5f3b51f8fca9a
supermarkioner <supermarkioner@gmail.com>
2016-03-22 08:35:46 +1100

    update

--- Paint House II.cpp
+++ Paint House II.cpp
@@ -14,5 +14,39 @@
 	
     Solution: None
 
-    Source: None
+    Source: http://www.cnblogs.com/easonliu/p/4784858.html
 */
+
+class Solution {
+public:
+    /**
+     * @param costs n x k cost matrix
+     * @return an integer, the minimum cost to paint all houses
+     */
+    int minCostII(vector<vector<int>>& costs) {
+        // Write your code here
+        if (costs.empty() || costs[0].empty()) {
+        	return 0;	
+        }
+        int n = costs.size(), k = costs[0].size();
+        vector<int> min1(k), min2(k);
+        for (int i = 1; i < costs.size(); ++i) {
+            min1[0] = INT_MAX;
+            for (int j = 1; j < k; ++j) {
+                min1[j] = min(min1[j - 1], costs[i - 1][j - 1]);
+            }
+            min2[k-1] = INT_MAX;
+            for (int j = k - 2; j >= 0; --j) {
+                min2[j] = min(min2[j + 1], costs[i - 1][j + 1]);
+            }
+            for (int j = 0; j < k; ++j) {
+                costs[i][j] += min(min1[j], min2[j]);
+            }
+        }
+        int res = INT_MAX;
+        for (auto c : costs.back()) {
+            res = min(res, c);
+        }
+        return res;
+    }
+};
