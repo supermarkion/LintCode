7d23e3ddc7769171b40a29812a088b3690a9b8f0
supermarkioner <supermarkioner@gmail.com>
2016-03-24 08:53:01 +1100

    update

---
+++ Super Ugly Number.cpp
@@ -0,0 +1,15 @@
+/*
+    Write a program to find the nth super ugly number. Super ugly numbers are positive numbers whose all 
+    prime factors are in the given prime list primes of size k. For example, [1, 2, 4, 7, 8, 13, 14, 16, 
+    19, 26, 28, 32] is the sequence of the first 12 super ugly numbers given primes = [2, 7, 13, 19] of 
+    size 4.
+   
+	Link: http://www.lintcode.com/en/problem/super-ugly-number/
+
+    Example: 
+	    Given n = 6, primes = [2, 7, 13, 19] return 13
+	
+    Solution: None
+
+    Source: None
+*/
